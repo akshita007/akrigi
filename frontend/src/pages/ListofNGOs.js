b66dfd79d011ed34749c946ad6e6ccdbfa0195fc
Akshita Sahni <akshitasahni97@gmail.com>
2022-05-03 21:50:38 +0530

    dynamic ngos added to list

--- frontend/src/pages/ListofNGOs.js
+++ frontend/src/pages/ListofNGOs.js
@@ -1,41 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container,Row,Col } from 'react-bootstrap'
 import {AiFillHome} from 'react-icons/ai';
 import {BsFillTelephoneFill} from 'react-icons/bs';
 import {GiLaptop} from 'react-icons/gi';
+import { ngoList } from '../services/api';
 
 const ListofNGOs = () => {
+    const [ngolst,setngolst]=useState([]);
+    const fetchLst = async()=>{
+        const data =await ngoList();
+        const ngolst=data;
+        setngolst(ngolst);
+    };
+
+    useEffect(()=>{
+        fetchLst();
+    },[])
   return (
     <div>
         <h1 className='text-center my-4 '>List of NGOs</h1>
         <Container>
-            <Row className='my-5'>
-                <Col className='mt-2'>
-                    <h4 style={{color:'#b95250'}}><b>Smile India</b></h4>
-                    <AiFillHome className='ngoicon'/>Dr. Vikram Sarabhai Road, Behind ATIRA, Ahmedabad 380015<br/>
-                    <BsFillTelephoneFill className='ngoicon'/>079-2630 8156<br/>
-                    <GiLaptop className='ngoicon'/><a href='https://smileindia.com/' className='text-lowercase'>https://smileindia.com/</a>
-                </Col>
-                <Col className='mt-2'>
-                <h4 style={{color:'#b95250'}}><b>Smile India</b></h4>
-                    <AiFillHome className='ngoicon'/>Dr. Vikram Sarabhai Road, Behind ATIRA, Ahmedabad 380015<br/>
-                    <BsFillTelephoneFill className='ngoicon'/>079-2630 8156<br/>
-                    <GiLaptop className='ngoicon'/><a href='https://smileindia.com/' className='text-lowercase'>https://smileindia.com/</a>
-                </Col>
-            </Row>
-            <Row className='my-5'>
-                <Col className='mt-2'>
-                    <h4 style={{color:'#b95250'}}><b>Smile India</b></h4>
-                    <AiFillHome className='ngoicon'/>Dr. Vikram Sarabhai Road, Behind ATIRA, Ahmedabad 380015<br/>
-                    <BsFillTelephoneFill className='ngoicon'/>079-2630 8156<br/>
-                    <GiLaptop className='ngoicon'/><a href='https://smileindia.com/' className='text-lowercase'>https://smileindia.com/</a>
-                </Col>
-                <Col className='mt-2'>
-                <h4 style={{color:'#b95250'}}><b>Smile India</b></h4>
-                    <AiFillHome className='ngoicon'/>Dr. Vikram Sarabhai Road, Behind ATIRA, Ahmedabad 380015<br/>
-                    <BsFillTelephoneFill className='ngoicon'/>079-2630 8156<br/>
-                    <GiLaptop className='ngoicon'/><a href='https://smileindia.com/' className='text-lowercase'>https://smileindia.com/</a>
-                </Col>
+            <Row xs={1} md={3}>
+                {ngolst.map((ngo)=>(
+                    <Col className="m-3">
+                        <h4 style={{color:'#b95250'}}><b>{ngo.username}</b></h4>
+                        <AiFillHome className='ngoicon'/>{ngo.email}<br/>
+                        <BsFillTelephoneFill className='ngoicon'/>{ngo.mob}<br/>
+                        <GiLaptop className='ngoicon'/><a href="/" className='text-lowercase'>{ngo.site}</a>
+                    </Col>
+                    ))}
             </Row>
         </Container>
     </div>
