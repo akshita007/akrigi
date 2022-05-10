4b534425d52b0386747e15cec7774833ad03f4dc
Akshita Sahni <akshitasahni97@gmail.com>
2022-05-10 15:46:11 +0530

    Single product page setup

--- frontend/src/pages/SingleProd.js
+++ frontend/src/pages/SingleProd.js
@@ -1,6 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Carousel } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { productItem } from '../services/api';
+import { addProduct } from "../Redux/CartRedux";
+import { useDispatch } from "react-redux";
 
 const SingleProd = () => {
+    const [item , setItem] = useState({
+        _id:'',
+        name:'',
+        category:'',
+        desc:'',
+        images:[],
+        price:0,
+        seller:'',
+    });
+    const fetchPost = async ()=>{
+        let data = await productItem(id);
+        console.log(data.seller);
+        setItem(data);
+    }
+
+    const dispatch = useDispatch();
+
+    const {id}=useParams();
+
+    useEffect(()=>{ 
+        fetchPost();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
+    
+    const handleClick = () => {
+        if(localStorage.getItem("token")!==null){
+            dispatch(
+              addProduct({...item})
+            );
+            alert("Product Added!");
+            window.location.replace("/cart");
+    }else{
+        alert("Login First!");
+    }
+    };
   return (
     
     <div className="container mt-5 mb-5">
@@ -10,24 +50,38 @@ const SingleProd = () => {
                 <div className="row">
                     <div className="col-md-6">
                         <div className="images p-3">
-                            <div className="text-center p-4"> <img id="main-image" src="https://i.imgur.com/Dhebu4F.jpg" width="250" /> </div>
-                            <div className="thumbnail text-center"> <img onclick="change_image(this)" src="https://i.imgur.com/Rx7uKd0.jpg" width="70" /> <img onclick="change_image(this)" src="https://i.imgur.com/Dhebu4F.jpg" width="70" /> </div>
-                        </div>
+                        <Carousel>
+                        {item.images.map((img)=>(
+                            <Carousel.Item>
+                                <div className = "slide" style={{backgroundColor:"#F2EFEA"}}>
+                                    <img src ={`/productImages/${img.filename}`} alt="slide" width="650px" height="500px"/>
+                                </div>      
+                            </Carousel.Item>))}
+                        </Carousel>
                     </div>
-                    <div className="col-md-6">
+                    </div>
+                    <div className="mt-5 col-md-6">
                         <div className="product p-4">
                             <div className="d-flex justify-content-between align-items-center">
                                 <div className="d-flex align-items-center"> <i className="fa fa-long-arrow-left"></i> <span className="ml-1">Back</span> </div> <i className="fa fa-shopping-cart text-muted"></i>
                             </div>
-                            <div className="mt-4 mb-3"> <span className="text-uppercase text-muted brand">Orianz</span>
-                                <h5 className="text-uppercase">Men's slim fit t-shirt</h5>
-                                <div className="price d-flex flex-row align-items-center"> <span className="act-price">$20</span>
-                                    <div className="ml-2"> <small className="dis-price">$59</small> <span>40% OFF</span> </div>
+                            <div className="mt-4 mb-3"> <span className="text-uppercase text-muted brand">{item.category}</span>
+                                <h5 className="text-uppercase">{item.name}</h5>
+                                <div className="price d-flex flex-row align-items-center"> <span className="act-price">Rs.{item.price}</span>
+                                    <div className="ml-2"> <small className="dis-price">{item.price}</small> <span>40% OFF</span> </div>
                                 </div>
+                                <h5 className="text-uppercase mt-3">Product Description: </h5>
+                                <p className="about">{item.desc}</p>
                             </div>
-                            <p className="about">Shop from a wide range of t-shirt from orianz. Pefect for your everyday use, you could pair it with a stylish pair of jeans or trousers complete the look.</p>
-                            
-                            <div className="cart mt-4 align-items-center"> <button className="btn btn-danger text-uppercase mr-2 px-4">Add to cart</button> <i className="fa fa-heart text-muted"></i> <i className="fa fa-share-alt text-muted"></i> </div>
+                            <div className='mt-4'>
+                            <h5 className="text-uppercase">Seller Info:</h5>
+                            <p className="about">Name: {item.seller.firstName} {item.seller.lastName}</p>
+                            <p className="about text-lowercase">Email: {item.seller.email} </p>
+                            <p className="about">Posted On: {new Date(item.seller.createdAt).getDate()+"/"+new Date(item.seller.createdAt).getMonth()+"/"+new Date(item.seller.createdAt).getFullYear()} </p>
+                            </div>
+                            <div className="cart mt-4 align-items-center">
+                                <button className="btn btn-danger text-uppercase mr-2 px-4" onClick={handleClick}>Add to cart</button>
+                            <i className="fa fa-heart text-muted"></i> <i className="fa fa-share-alt text-muted"></i> </div>
                         </div>
                     </div>
                 </div>
@@ -39,4 +93,4 @@ const SingleProd = () => {
   )
 }
 
-export default SingleProd
+export default SingleProd;
