1df6ec02c98a08b1515f0cb958cb0112d33e456d
Akshita Sahni <akshitasahni97@gmail.com>
2022-05-11 22:04:10 +0530

    get order by id modify

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -47,7 +47,7 @@ router.get("/find/:userId",verify.verifyUser, async (req,res)=>{
   try{
     const orders = await Order.find({userId: req.params.userId});
 
-    res.status(200).json(cart);
+    res.status(200).json(orders);
   }catch(err){
     res.status(500).json(err);
   }
