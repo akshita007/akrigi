1197d38d3c0398ba5cad618a7b6c6e2d7a718388
Akshita Sahni <akshitasahni97@gmail.com>
2022-05-17 18:00:16 +0530

    cart,success,orders service added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes,Route,Navigate} from "react-router-dom";
 import './App.css';
 import Navbartask from "./components/Navbar.js";
 import Footer from "./components/footer.js";
@@ -12,7 +12,7 @@ import {logoutUser, verifyToken} from "./services/api";
 import SellingForm from "./pages/SellForm";
 import Aboutus from "./pages/Aboutus";
 import Contactus from "./pages/Contactus";
-import DonationForm from "./pages/Donate";
+import DonationForm from "./pages/DonationForm";
 import NGORegisterationForm from "./pages/NGORegistrationForm";
 import Donate from "./pages/DonateCover";
 import MainContent from "./pages/MainContent";
@@ -21,8 +21,12 @@ import Returnpolicy from "./pages/Returnpolicy";
 import FAQs from "./pages/FAQs"
 import ListofNGOs from './pages/ListofNGOs';
 import SingleProd from "./pages/SingleProd";
+import Cart from "./pages/Cart";
+import Success from "./pages/Success";
+import Orders from "./pages/Orders";
 
 function App() {
+  
   useEffect(()=>{
     const checkToken = async()=>{
       if(localStorage.getItem("token")!==null){
@@ -37,7 +41,6 @@ function App() {
   },[])
   return (
   <Router>
-    <Routes><Route exact path="/" element = {<Animation/>}></Route></Routes>
     <Navbartask/>
       <Routes>
         <Route exact path="/" element = {<Animation/>}></Route>
@@ -46,16 +49,20 @@ function App() {
         <Route path="/register" element = {<Register/>}></Route>
         <Route path="/aboutus" element = {<Aboutus/>}></Route>
         <Route path="/contactus" element = {<Contactus/>}></Route>
-        <Route path="/sellForm" element = {<SellingForm/>}></Route>
-        <Route path="/donateForm" element = {<DonationForm/>}></Route>
-        <Route path="/registerngo" element = {<NGORegisterationForm/>}></Route>
+        <Route path="/sellForm" element={(localStorage.getItem("token")!==null)?(<SellingForm/>):(<Navigate reload to="/register"/>)}></Route>
         <Route path="/donate" element = {<Donate/>}></Route>
+        <Route path="/donateForm" element = {localStorage.getItem("token")!==null?<DonationForm/>:(<Navigate reload to="/register"/>)}></Route>
+        <Route path="/registerngo" element = {<NGORegisterationForm/>}></Route>
         <Route path="/termsandcondition" element = {<TC/>}></Route>
         <Route path="/listofNGOs" element={<ListofNGOs/>}></Route>
         <Route path="/returnpolicy" element={<Returnpolicy/>}></Route>
-        <Route path="/MainContent" element= {<MainContent/>}></Route>
+        <Route path="/success" element= {(localStorage.getItem("token")!==null)?<Success/>:(<Navigate reload to="/homepage"/>)}></Route>
+        <Route path="/MainContent/:category" element= {<MainContent/>}></Route>
+        <Route path="/MainContent/:price" element= {<MainContent/>}></Route>
+        <Route path="/product/:id" element={<SingleProd/>}></Route>        
         <Route path="/faqs" element={<FAQs/>}></Route>
-       <Route path="/singleproduct" element={<SingleProd/>}></Route>
+        <Route path="/cart" element={(localStorage.getItem("token")!==null)?<Cart/>:(<Navigate reload to="/homepage"/>)}></Route>
+        <Route path="/myOrder" element={(localStorage.getItem("token")!==null)?<Orders/>:(<Navigate reload to="/homepage"/>)}></Route>
       </Routes>
     <Footer/>
   </Router>
