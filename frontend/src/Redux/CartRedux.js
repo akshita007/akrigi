3a551ae903f93e4c4ac4435835630f19d5d3cdb0
Akshita Sahni <akshitasahni97@gmail.com>
2022-05-17 18:01:37 +0530

    redux improved

--- frontend/src/Redux/CartRedux.js
+++ frontend/src/Redux/CartRedux.js
@@ -8,13 +8,13 @@ const cartSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
-      const duplicate=state.products.find(item=>item.id===action.payload.id);
-      if(!duplicate){
+      // const duplicate=state.products.find(item=>item.id===action.payload.id);
+      // if(!duplicate){
         state.products.push(action.payload);
         state.total += action.payload.price;
-    }else{
-      return state
-    }
+    // }else{
+    //   return state
+    // }
     },
     removeProduct:(state,action)=>{
       const updatedCart=state.products.filter(item=>item.id!==action.payload.id);
